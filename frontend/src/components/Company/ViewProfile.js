b45a663ba3cd47db60dd35d851cb97b91011de95
sreejasriram <sreeja.sriram@sjsu.edu>
2020-04-04 15:42:07 -0700

    frontend view profile
    view student profile from both students and companies

--- frontend/src/components/Company/ViewProfile.js
+++ frontend/src/components/Company/ViewProfile.js
@@ -12,51 +12,43 @@ import Typography from '@material-ui/core/Typography';
 import {environment} from '../../Utils/constants';
 
 
-const useStyles = makeStyles({
-    root: {
-        minWidth: 275,
-    },
-    bullet: {
-        display: 'inline-block',
-        margin: '0 2px',
-        transform: 'scale(0.8)',
-    },
-    title: {
-        fontSize: 14,
-    },
-    pos: {
-        marginBottom: 12,
-    },
-});
-
-
-///
 class ViewProfile extends Component {
     constructor(props) {
         super(props);
         this.state = {
             stud_id: this.props.match.params.stud_id,
             name: "",
-            date: "",
+            dob: "",
             city: "",
             state: "",
             country: "",
-            career_obj: "",
-            skill: "",
-            phone: "",
+            career_objective: "",
+            skills: "",
+            mobile: "",
             email: "",
-            clg: "",
-            loc: "",
+            college:"",
+            education:[
+
+
+            ],
+            experience:[],
+            college_name: "",
+            edulocation: "",
             degree: "",
             major: "",
-            year: "",
+            year_of_starting: "",
+            year_of_passing:"",
+            month_of_starting: "",
+            month_of_passing:"",
             cgpa: "",
-            cmpy_name: "",
+            company: "",
             title: "",
-            cmpyloc: "",
-            start_date: "",
-            end_date: "",
-            work_desc: "",
+            location: "",
+            year_of_starting: "",
+            year_of_ending: "",
+            month_of_starting: "",
+            month_of_ending: "",
+            description: "",
             dataRetrieved: false,
             profileData: []
         }
@@ -69,7 +61,7 @@ class ViewProfile extends Component {
             id: this.state.stud_id
         }
         console.log(data)
-        axios.post(environment.baseUrl+'/company/get_student_profile', data)
+        axios.get(environment.baseUrl+'/company/get_student_profile/'+data.id)
             .then(response => {
                 console.log("in frontend after response");
                 console.log(response.data.rows)
@@ -78,25 +70,27 @@ class ViewProfile extends Component {
                         dataRetrieved: true,
                         profileData: response.data.rows,
                         name: response.data.rows[0].name,
-                        date: response.data.rows[0].dob,
+                        dob: response.data.rows[0].dob,
                         city: response.data.rows[0].city,
                         state: response.data.rows[0].state,
                         country: response.data.rows[0].country,
-                        career_obj: response.data.rows[0].career_obj,
-                        skill: response.data.rows[0].skill,
-                        phone: response.data.rows[0].phone,
+                        career_objective: response.data.rows[0].career_objective,
+                        skills: response.data.rows[0].skills,
+                        mobile: response.data.rows[0].mobile,
                         email: response.data.rows[0].email,
-                        clg: response.data.rows[0].clg,
-                        loc: response.data.rows[0].loc,
-                        degree: response.data.rows[0].degree,
-                        major: response.data.rows[0].major,
-                        year: response.data.rows[0].year,
-                        cgpa: response.data.rows[0].cgpa,
-                        cmpy_name: response.data.rows[0].cmpy_name,
-                        title: response.data.rows[0].title,
-                        start_date: response.data.rows[0].start_date,
-                        end_date: response.data.rows[0].end_date,
-                        work_desc: response.data.rows[0].work_desc
+                        college: response.data.rows[0].college,
+                        education: response.data.rows[0].education,
+                        experience:response.data.rows[0].experience
+                        // loc: response.data.rows[0].loc,
+                        // degree: response.data.rows[0].degree,
+                        // major: response.data.rows[0].major,
+                        // year: response.data.rows[0].year,
+                        // cgpa: response.data.rows[0].cgpa,
+                        // cmpy_name: response.data.rows[0].cmpy_name,
+                        // title: response.data.rows[0].title,
+                        // start_date: response.data.rows[0].start_date,
+                        // end_date: response.data.rows[0].end_date,
+                        // work_desc: response.data.rows[0].work_desc
                     });
                 } else if (response.data.error) {
                     console.log("response" + response.data.error)
@@ -123,14 +117,12 @@ class ViewProfile extends Component {
                                             <div class="col-md-10">
                                             <Typography color="black" gutterBottom>
                                                 <b><p style={{ fontSize: '24px' }}>{this.state.name}</p></b>
-
                                             </Typography>
                                             </div>
-                                            
                                             </div>
-                                            {/* <p>{this.state.dob.substring(0, 10)}</p> */}
+                                            <p>{this.state.dob}</p>
                                             <p>{this.state.city} {this.state.state} {this.state.country} </p>
-                                            <p>{this.state.career_obj}</p>
+                                            <p>{this.state.career_objective}</p>
 
                                         </CardContent>
                                     </Card>
@@ -146,9 +138,9 @@ class ViewProfile extends Component {
                                             <b><p style={{ fontSize: '24px' }}>Skills</p></b>
                                             </Typography>
                                             </div>
-                                            
+                            
                                             </div>
-                                    <p>{this.state.skill}</p>
+                                    <p>{this.state.skills}</p>
                                     </CardContent>
                                         </Card>
                                     <br />
@@ -166,15 +158,23 @@ class ViewProfile extends Component {
                                         </div>
                                        
                                     </div>
-                                    <p>{this.state.clg}</p>
-                                    <p>{this.state.loc}</p>
-                                    <p>{this.state.degree} {this.state.major} {this.state.year} </p>
+                                   { this.state.education.map((data,index)=>{
+                                       return(
+                                           <div>
+                                        <p>{data.college_name}</p>
+                                        <p>{data.location}</p>
+                                        <p>{data.degree} {data.major} </p>
+                                        <p>{data.year_of_starting}/{data.month_of_starting}-{data.year_of_passing}/{data.month_of_passing}</p>
+                                        <hr/>
+                                        </div> )
+                                    })}
+                                   
                                 </CardContent>
                             </Card>
                             <br/>
 
                             {/*Experience */}
-                            <Card>
+                            {/* <Card>
                                 <CardContent>
                                     <div class="row">
                                         <div class="col-md-10">
@@ -185,11 +185,11 @@ class ViewProfile extends Component {
                                         </div>
 
                                     </div>
-                                    <p>{this.state.title} at {this.state.cmpy_name}</p>
-                                    <p>{this.state.loc}</p>
+                                    <p>{this.state.title} at {this.state.company}</p>
+                                    <p>{this.state.location}</p>
                                     <p>{this.state.start_date.substring(0, 10)} - {this.state.end_date.substring(0, 10)}</p>
                                 </CardContent>
-                            </Card>
+                            </Card> */}
                             {/* Contact  */}
                                 <br/>
                             <Card>
@@ -203,7 +203,7 @@ class ViewProfile extends Component {
                                             </div>
                                            
                                             </div>
-                                    <p>{this.state.phone}</p>
+                                    <p>{this.state.mobile}</p>
                                     <p>{this.state.email}</p>
                                     </CardContent>
                                         </Card>
@@ -211,31 +211,7 @@ class ViewProfile extends Component {
                         </div>
                         <div class="col-md-1"> </div>
                     </div>
-                    {/* <Card>
-                        <CardContent>
-                            <Typography color="textSecondary" gutterBottom>
-                                <h2>{this.state.name}</h2>
-                            </Typography>
-                            <h3>{this.state.date}</h3>
-                            <p>{this.state.city} {this.state.state} {this.state.country} </p>
-                            <p>{this.state.career_obj}</p>
-                            {this.state.skill}
-                            {this.state.phone}
-                            {this.state.email}
-                            {this.state.clg}
-                            {this.state.loc}
-                            {this.state.degree}
-                            {this.state.major}
-                            {this.state.year}
-                            {this.state.cgpa}
-                            {this.state.cmpy_name}
-                            {this.state.title}
-                            {this.state.cmpyloc}
-                            {this.state.start_date}
-                            {this.state.end_date}
-                            {this.state.work_desc}
-                        </CardContent>
-                    </Card> */}
+                 
                     <br /><br />
                 </div>
         }
